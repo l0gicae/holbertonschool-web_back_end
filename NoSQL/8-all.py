e2721dc3c7cd2860e835e25fc390d9469090d4a9
Farid Mehtizada <107333073+l0gicae@users.noreply.github.com>
2025-12-26 01:44:30 +0400

    Create 8-all.py

---
+++ NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""List all documents in Python module."""
+
+
+def list_all(mongo_collection):
+    """List all documents in a collection."""
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
