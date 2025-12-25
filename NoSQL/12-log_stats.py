43e57bfd0e055dfaebd846507bdb1013986abf45
Farid Mehtizada <107333073+l0gicae@users.noreply.github.com>
2025-12-26 01:52:44 +0400

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,24 +2,24 @@
 """Log stats module."""
 from pymongo import MongoClient
 
-
 if __name__ == "__main__":
+    # MongoDB-yə qoşulma
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    # 1. Total logs
-    total_logs = nginx_collection.count_documents({})
-    print("{} logs".format(total_logs))
+    # 1. Ümumi sənəd sayını tapmaq
+    n_logs = nginx_collection.count_documents({})
+    print(f"{n_logs} logs")
 
-    # 2. Methods stats
+    # 2. Metodlar üzrə statistika
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+        print(f"\tmethod {method}: {count}")
 
-    # 3. Status check
+    # 3. Status check (method=GET və path=/status) sayını tapmaq
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print("{} status check".format(status_check))
+    print(f"{status_check} status check")
